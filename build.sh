dbd405720c9d25f86f430b5a8c8328dab007a932
Igor Zboichik <zboigor@gmail.com>
2020-06-24 12:59:26 +0300

    Fixed

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ export GRAALVM_HOME=/Users/igorz/.sdkman/candidates/java/graalvm-ce-java11-20.2.
 export JAVA_HOME=$GRAALVM_HOME
 
 mvn clean install
-java -agentlib:native-image-agent=config-merge-dir=./reflection-info -jar ./target/jaxb.jar
+java -agentlib:native-image-agent=config-merge-dir=./reflection-info,caller-filter-file=./filter.json -jar ./target/jaxb.jar
 ./target/jaxb

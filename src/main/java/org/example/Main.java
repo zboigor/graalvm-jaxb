ba06bd26d721c4d754f4cb1b4d43704f8db0f5ef
Igor Zboichik <zboigor@gmail.com>
2020-06-24 11:48:20 +0300

    Fixed config issues

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,11 +6,13 @@ import javax.xml.bind.JAXBException;
 
 public class Main {
 
+    public static final String PROPERTIES_PATH = "./properties.xml";
+
     public static void main(String[] args) {
         final JaxbProcessor jaxbProcessor = new JaxbProcessor();
 
         try {
-            System.out.println(jaxbProcessor.loadProperties("./properties.xml"));
+            System.out.println(jaxbProcessor.loadProperties(PROPERTIES_PATH));
         } catch (JAXBException e) {
             e.printStackTrace();
         }
